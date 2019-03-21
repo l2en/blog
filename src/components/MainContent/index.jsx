10a721ce88e32a42041bf5dbd2f7d9d12a92b317
luolin <luolin@luolindeMBP.lan>
2019-03-21 11:20:29 +0800

    切换菜单样式

--- src/components/MainContent/index.jsx
+++ src/components/MainContent/index.jsx
@@ -4,26 +4,58 @@ import './index.scss';
 
 class MainContent extends Component {
   state = {
-    projects: [1, 23, 3, 3, 321, 321, 21]
+    projects: [1, 23, 3, 3, 321, 321, 21],
+    navs: [
+      {
+        name: 'Overview',
+        num: 0
+      },
+      {
+        name: 'Repoositories',
+        num: 0
+      },
+      {
+        name: 'Projects',
+        num: 10
+      },
+      {
+        name: 'Starts',
+        num: 121
+      },
+      {
+        name: 'Fillowers',
+        num: 9
+      },
+      {
+        name: 'Following',
+        num: 12
+      }
+    ],
+    activeNav: 'Overview'
+  
   }
-
+  changeNav=(navname)=>{
+    this.setState({activeNav:navname})
+  }
   render() {
-    const { projects } = this.state;
-    
+    const { projects, navs, activeNav } = this.state;
+
     return (
       <div className='main-right-container fl'>
         <nav className='main-right-nav'>
-          <span className='main-right-item navActie'>Overview</span>
-          <span className='main-right-item'>Repoositories<span className='main-right-item-counter'>23</span></span>
-          <span className='main-right-item'>Projects</span>
-          <span className='main-right-item'>Starts<span className='main-right-item-counter'>3</span></span>
-          <span className='main-right-item'>Fillowers</span>
-          <span className='main-right-item'>Following</span>
+          {
+            navs.map(item =>
+              <span key={item.name} className={activeNav===item.name?'navActie main-right-item': 'main-right-item'} onClick={()=>this.changeNav(item.name)}>{item.name}
+                {Number(item.num) ? <span className='main-right-item-counter'>{item.num}</span> : null}
+              </span>
+
+            )
+          }
         </nav>
         <div className='main-right-main'>
           <h2 className='main-right-main-title'>Popular repositories </h2>
           <div className='main-right-main-projects'>
-          <ContentItem projects={projects}/>
+            <ContentItem projects={projects} />
           </div>
         </div>
       </div>
