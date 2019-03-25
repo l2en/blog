6ec001435e5f52ce56eff316d1bbf8f6da68c87e
luolin <luolin@luolindeMBP.lan>
2019-03-25 17:13:42 +0800

    footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -25,8 +25,8 @@ class Footer extends Component {
 		return (
 			<footer className='footer'>
 				<span>
-					<span className='footer-task'><span className='fw-600'>已稳定运行:</span> <em>{taskTime}</em></span>
-					<span className='ml-10 fw-600'> |&nbsp;&nbsp;Personal Projects:</span>
+					<span><span className='fw-600'>已稳定运行:</span><em className='footer-task-time'>{taskTime}</em></span>
+					<span className='ml-10 fw-600'>|&nbsp;&nbsp;Personal Projects:</span>
 					<a href="https://github.com/l2en" title='基于网易云音乐接口开发的在线音乐原生微信小程序' className='footer-link' target='_blank'>netMusic</a>
 					<a href="https://github.com/l2en" title='个人开发记账小程序' className='footer-link' target='_blank'>iWallet</a>
 				</span>
@@ -39,7 +39,7 @@ class Footer extends Component {
 					<span className='ml-10 fw-600'>Commercial Projects:</span>
 					<a href="https://github.com/l2en" title='商业项目,NeuronApp/Neuron面端/NeuronPC' className='footer-link' target='_blank'>Neuron系列</a>
 					<a href="https://github.com/l2en" className='footer-link' target='_blank'>纽睿CRM</a>
-					<span className='footer-views ml-10'><span className='fw-600'> |&nbsp;&nbsp;访问总人次:</span><em>12345</em></span>
+					<span className='footer-views ml-10'><span className='fw-600'>|&nbsp;&nbsp;访问总人次:</span><em>12345</em></span>
 				</span>
 			</footer>
 		)
