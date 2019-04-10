b54c381cdbad97cb7b98e574ffeb0ad2dffb34be
luolin <luolin@luolindeMBP.lan>
2019-04-10 15:25:55 +0800

    添加二维码

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,33 +1,41 @@
 import React, { Component } from 'react';
 import Moment from 'moment';
+import { Popover } from 'antd';
+import "antd/dist/antd.css";
 import './index.scss';
 
 class Footer extends Component {
 	state = {
 		taskTime: '--天--小时--分钟--秒'
 	}
-	componentDidMount(){
+	componentDidMount() {
 		this.countTakTime();
 	}
 	// 运行时间计算
-	countTakTime = ()=>{
+	countTakTime = () => {
 		const _this = this
-		setInterval(()=>{
+		setInterval(() => {
 			let startTimeUnix = Moment('2019-3-1 17:10:00').valueOf();
 			let nowUnix = Moment().valueOf();
 			let taskTime = Moment(Moment(nowUnix).diff(startTimeUnix)).format('DD天hh小时mm分钟ss秒')
-			_this.setState({taskTime})
-		},1000)
+			_this.setState({ taskTime })
+		}, 1000)
 	}
 	render() {
-		const {taskTime} = this.state;
-
+		const { taskTime } = this.state;
+		const content = (
+			<div>
+				<img src='http://t.cn/E6jLVkv' style={{width:' 200px',height:'200px'}}/>
+			</div>
+		);
 		return (
 			<footer className='footer'>
 				<span>
 					<span><span className='fw-600'>已稳定运行:</span><em className='footer-task-time'>{taskTime}</em></span>
 					<span className='ml-10 fw-600'>|&nbsp;&nbsp;Personal Projects:</span>
-					<a href="https://github.com/l2en" title='基于网易云音乐接口开发的在线音乐原生微信小程序' className='footer-link' rel="noopener noreferrer" target='_blank'>netMusic</a>
+					<Popover content={content} title="" trigger="hover">
+						<a href="https://github.com/l2en" title='基于网易云音乐接口开发的在线音乐原生微信小程序' className='footer-link' rel="noopener noreferrer" target='_blank'>netMusic</a>
+					</Popover>
 					<a href="https://github.com/l2en" title='个人开发记账小程序' className='footer-link' rel="noopener noreferrer" target='_blank'>iWallet</a>
 				</span>
 				<a href="https://github.com/l2en" rel="noopener noreferrer" target='_blank'>
